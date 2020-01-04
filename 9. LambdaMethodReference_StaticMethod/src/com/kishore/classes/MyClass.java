65fa1fde64552689af2057c576ec3df7ffede1f0
Kishore Kumar <kishorekumar5464@gmail.com>
2020-01-04 23:06:58 +0530

    Small change in 9 method reference

--- 9. LambdaMethodReference_StaticMethod/src/com/kishore/classes/MyClass.java
+++ 9. LambdaMethodReference_StaticMethod/src/com/kishore/classes/MyClass.java
@@ -4,12 +4,12 @@ import com.kishore.interfaces.MyInterface;
 
 public class MyClass {
 	public static void main(String[] args) {
-		MyInterface interface1 = MyClass::myCMethod;
-		interface1.myIMethod();
+		MyInterface interface1 = MyClass::myClassMethod;
+		String message = interface1.myIMethod();
+		System.out.println(message);
 	}
 
-	public static String myCMethod() {
-		System.out.println("Hai");
-		return null;
+	public static String myClassMethod() {
+		return "Hello";
 	}
 }
